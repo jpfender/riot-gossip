59eb6934381c6ef481eb239dcdcafbcd2529613e
Jakob Pfender <jpfender@zedat.fu-berlin.de>
2014-01-14 17:47:50 +0100

    leader: Implement round-based leader election

--- src/leader.c
+++ src/leader.c
@@ -11,6 +11,7 @@
 #include "debug.h"
 
 uint16_t leader=0;
+uint16_t election_round=0;
 
 uint16_t leader_elect(uint16_t source){
     int i=0;
@@ -19,7 +20,7 @@ uint16_t leader_elect(uint16_t source){
 
     leader=source;
     for(i=0;i<ROUNDS;i++){
-        sprintf(msg_buffer, "%s%s%s%i", PREAMBLE, MSG, LE, leader);
+        sprintf(msg_buffer, "%s%s%s%03i%i", PREAMBLE, MSG, LE, election_round, leader);
         node = gossip_get_neighbour(RANDOM);
         if(!node){
             DEBUG("Warning: no neighbours, election failed.\n");
@@ -33,10 +34,17 @@ uint16_t leader_elect(uint16_t source){
 
 void leader_handle_msg(void* msg_text, size_t size, uint16_t src){
     uint16_t received_leader;
+    uint16_t round;
     gossip_node_t* node;
     char msg_buffer[strlen(PREAMBLE) + strlen(MSG) + strlen(LE) + 100];
 
-    received_leader = atol((char*)msg_text+strlen(LE));
+    round = atol((char*)msg_text+strlen(LE));
+    if(round > election_round || election_round - round > 128)
+        // A new leader election round has been started; discard old
+        // leader and assume I am the leader
+        leader = gossip_id;
+
+    received_leader = atol((char*)msg_text+strlen(LE)+3);
     printf("received candidate: %i\n",received_leader);
 
     // if new message contains worse leader candidate, inform node directly
@@ -44,7 +52,7 @@ void leader_handle_msg(void* msg_text, size_t size, uint16_t src){
 #if 0
     if(received_leader < leader ){
         DEBUG("discarding candidate and informing sender\n");
-        sprintf(msg_buffer, "%s%s%s%i", PREAMBLE, MSG, LE, leader);
+        sprintf(msg_buffer, "%s%s%s%03i%i", PREAMBLE, MSG, LE, round, leader);
         node = gossip_find_node_by_id(src);
         gossip_send(node, msg_buffer, strlen(msg_buffer));
     }
@@ -54,5 +62,11 @@ void leader_handle_msg(void* msg_text, size_t size, uint16_t src){
         DEBUG("adding a new, better leader\n");
         leader = received_leader;
     }
+
+    if(round > election_round || election_round - round > 128) {
+        election_round = round;
+        leader_elect(leader);
+    }
+
     printf("%i\n",leader);
 }
