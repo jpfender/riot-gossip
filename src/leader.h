2ba5c12d700056dd9d4b224a4cffaedc22da04f2
Jakob Pfender <jpfender@zedat.fu-berlin.de>
2014-01-28 13:54:39 +0100

    leader: Make getters and setters visible

--- src/leader.h
+++ src/leader.h
@@ -5,6 +5,12 @@
 #define LEADER_STACK_SIZE   (16384)
 
 
+char leader_get_active();
+void leader_set_active(char value);
+uint16_t leader_set_leader(uint16_t new_leader);
+uint16_t leader_get_leader();
+char leader_set_initialized(char v);
+char leader_get_initialized();
 int leader_init();
 int leader_elect();
 void leader_handle_msg(void*,size_t,uint16_t);
