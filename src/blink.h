a2e9209834780020b2fa3ceb891cdeebf22aae7a
mk <mk@spline.de>
2014-02-05 23:03:52 +0100

    fixup: past ballmer peak

--- src/blink.h
+++ src/blink.h
@@ -2,6 +2,8 @@
 #define BLINK_PAUSE     (1000*1000)
 #define BLINK_STACK_SIZE MINIMUM_STACK_SIZE
 
+int blink_pid;
+
 /* emulate a cron-like behaviour to fire at specific time
    this is a bit silly, since we could just use sleep(DELAY)
    but we want to reset the hwtimer regularly */
